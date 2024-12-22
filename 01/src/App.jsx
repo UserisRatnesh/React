b8f7fff604c1a1ef79111c31fd8f34df45bf8c73
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-12-23 00:28:12 +0530

    Learned the structure of react app.

---
+++ 01/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from './Chai.jsx'
+function App() {
+
+    return (
+        <div>
+            <Chai></Chai>
+            <h1>Hello from React</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ 01/src/Chai.jsx
@@ -0,0 +1,5 @@
+export default function Chai() {
+    return <div>
+        <h1>This is chai.</h1>
+    </div>
+}
